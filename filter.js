18a611c3d558413f72214769e31ba054b2421c96
gowtham1194 <gowtham.s.ece.2022@snsce.ac.in>
2025-03-13 09:46:45 +0530

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,21 @@
+
+let age = [ 18 ,22, 23,36, 38,45, 73]
+
+let filtered = age.filter((value) => {
+    return value >= 20
+})
+
+console.log(filtered);
+
+
+ 
+ let fruits = ["Apple","Blueberry","Strawberry","Grapes" ,"Mosambi"]
+
+let filterd = fruits.filter((  fruits , index) => {
+     return index >= 2
+})
+
+console.log(filterd); 
+
+
+
